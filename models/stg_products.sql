b1b02c80fccbefff97642ed3d9bc709bb2104c6e
tunnelview <abrahamshibin1@gmail.com>
2025-01-15 21:30:49 +1100

    Create stg_products as a table

---
+++ models/stg_products.sql
@@ -0,0 +1,18 @@
+
+--stg_products
+
+
+{{ config(materialized='table') }}
+
+with stg_products as (SELECT
+    ProductID AS product_id,
+    Name AS product_name,
+    ProductNumber AS product_number,
+    ListPrice AS list_price,
+    ModifiedDate AS modified_date
+FROM
+    AdventureWorks2022.Production.Product
+WHERE
+    ListPrice > 0)
+
+    Select * from stg_products;
